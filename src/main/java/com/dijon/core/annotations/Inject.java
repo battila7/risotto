e3e415929583f89c53e0d524024cd2f1c336f15c
Attila Bagossy <bagossyattila@outlook.com>
2016-08-07 11:41:12 +0200

    Added the core annotation.

---
+++ src/main/java/com/dijon/core/annotations/Inject.java
@@ -0,0 +1,22 @@
+package com.dijon.core.annotations;
+
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Marks a constructor, a field or a setter method as a target for dependency injection. Dijon
+ * first looks for a properly annotated constructor. If that's not found the next step is finding
+ * {@code public} setter methods with this annotation. If no {@code public} setter methods are
+ * found, then Dijon looks for fields with the {@code Inject} annotation.
+ */
+@Target( { CONSTRUCTOR, FIELD, METHOD })
+@Retention(RUNTIME)
+@Documented
+public @interface Inject {
+}
---
+++ src/main/java/com/dijon/core/annotations/InjectSpecifier.java
@@ -0,0 +1,19 @@
+package com.dijon.core.annotations;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Signals that an annotation can be used to specify the class or instance to be injected.
+ */
+@Target( { PARAMETER, FIELD, METHOD })
+@Retention(RUNTIME)
+@Documented
+public @interface InjectSpecifier {
+}
