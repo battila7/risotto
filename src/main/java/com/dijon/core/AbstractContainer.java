33b8dbee95204e42c5e4d9bf706e8b23c4e490d1
Attila Bagossy <bagossyattila@outlook.com>
2016-08-07 12:31:02 +0200

    Started AbstractContainer.

---
+++ src/main/java/com/dijon/core/AbstractContainer.java
@@ -0,0 +1,35 @@
+package com.dijon.core;
+
+/**
+ * Abstract dependency container class that must be subclassed by custom dependency containers.
+ */
+public abstract class AbstractContainer {
+  /**
+   * Configures the contents of the container. By overriding this method, instances and child
+   * containers can be added to the container object.
+   */
+  protected abstract void configure();
+
+  /**
+   * Adds a child container to this container. The dependency resolution of the passed child
+   * container is deferred until the dependency resolution of the current container takes place.
+   *
+   * Note that if you wish to add two or more containers with the same type then use
+   * the {@link AbstractContainer#addChildContainer(Class, String)} method to avoid
+   * type collision.
+   * @param childContainer the new child container class
+   */
+  public void addChildContainer(Class<? extends AbstractContainer> childContainer) {
+    // TODO: Implement
+  }
+
+  /**
+   * Adds a child container to this container with the specified name. Behaves the same as {@link
+   * AbstractContainer#addChildContainer(Class)}.
+   * @param childContainer the new child container class
+   * @param name a unique name for the child container
+   */
+  public void addChildContainer(Class<? extends AbstractContainer> childContainer, String name) {
+    // TODO: Implement
+  }
+}
