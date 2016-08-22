afa185d9015a14e949595022a1678e134ce38542
Attila Bagossy <bagossyattila@outlook.com>
2016-08-22 22:23:03 +0200

    Documented io.risotto.binding.scope

--- src/main/java/io/risotto/binding/scope/PublicScope.java
+++ src/main/java/io/risotto/binding/scope/PublicScope.java
@@ -3,6 +3,11 @@ package io.risotto.binding.scope;
 import io.risotto.Container;
 import io.risotto.dependency.Dependency;
 
+/**
+ * {@code PublicScope} is the widest and the <b>default</b> scope. Bindings with
+ * public scope are visible to {@code getInstance} requests and to all ancestors
+ * of their origin container. They are also imported to all descendant containers.
+ */
 public class PublicScope extends Scope {
   @Override
   public boolean isImportAllowedTo(Container targetContainer) {
