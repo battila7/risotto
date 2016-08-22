3f6f88029bf0784018d73cddd1e91fc43d28a615
Attila Bagossy <bagossyattila@outlook.com>
2016-08-22 23:19:01 +0200

    Documented io.risotto.instantiation

--- src/main/java/io/risotto/instantiation/Instantiator.java
+++ src/main/java/io/risotto/instantiation/Instantiator.java
@@ -4,10 +4,29 @@ import io.risotto.dependency.Dependency;
 
 import java.util.List;
 
+/**
+ * Interface for object-instantiator classes. {@code Instantiator}s can be used to create and
+ * retrieve instances of a specific type.
+ * @param <T> the type of the objects to be instantiated
+ */
 public interface Instantiator<T> {
+  /**
+   * Gets an instance created by the {@code Instantiator}.
+   * @return an instance
+   */
   T getInstance();
 
+  /**
+   * Gets the actual {@code Instantiator} used by the current {@code Instantiator}. When there is
+   * no base instantiator, {@code null} should be returned.
+   * @return the base instantiator of the current instance
+   */
   Instantiator<T> getBaseInstantiator();
 
+  /**
+   * Gets the list of {@code Dependency} objects that should be resolved before object
+   * instantiation can take place. Only immediate dependencies are returned.
+   * @return the list of immediate dependencies
+   */
   List<Dependency<?>> getImmediateDependencies();
 }
--- src/main/java/io/risotto/instantiation/InstantiatorFactory.java
+++ src/main/java/io/risotto/instantiation/InstantiatorFactory.java
@@ -1,6 +1,19 @@
 package io.risotto.instantiation;
 
+/**
+ * Factory class that provides static methods to decorate instantiators with appropriate
+ * decorator implementations. That way the concrete implementations can be abstracted away from
+ * client code using the factory class.
+ */
 public final class InstantiatorFactory {
+  /**
+   * Returns a decorated instantiator using the specified base instantiator and the default decorator
+   * associated with the value of the {@code mode} parameter.
+   * @param instantiator the instantiator to decorate
+   * @param mode the mode of which a decorator is requested
+   * @param <T> the requested instance's type
+   * @return the decorated instantiator
+   */
   public static <T> Instantiator<T> decorateInstantiatorForMode(Instantiator<T> instantiator,
                                                                 InstantiationMode mode) {
     Instantiator<T> base = instantiator.getBaseInstantiator();
@@ -17,6 +30,12 @@ public final class InstantiatorFactory {
     }
   }
 
+  /**
+   * Returns a decorated instantiator decorated with the default decorator implementation.
+   * @param instantiator the instantiator to decorate
+   * @param <T> the requested instance's type
+   * @return the decorated instantiator
+   */
   public static <T> Instantiator<T> decorateWithDefaultInstantiator(Instantiator<T> instantiator) {
     Instantiator<T> base = instantiator.getBaseInstantiator();
 
