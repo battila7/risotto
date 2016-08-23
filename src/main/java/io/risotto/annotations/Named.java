a5b45c1a32d6fa87279ad04179dfa2a367a2628f
Attila Bagossy <bagossyattila@outlook.com>
2016-08-23 22:50:56 +0200

    Documented io.risotto.dependency

--- src/main/java/io/risotto/annotations/Named.java
+++ src/main/java/io/risotto/annotations/Named.java
@@ -12,8 +12,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
- * Marks an injection target to be injected with the specified named instance or class.
- *
+ * Marks an injection target to be injected with the specified named instance or class. This
+ * annotation can be placed on
+ * <ul>
+ *   <li>constructor parameters,</li>
+ *   <li>setter methods,</li>
+ *   <li>fields.</li>
+ * </ul>.
  * @see NamedBinding
  */
 @Target({PARAMETER, FIELD, METHOD})
