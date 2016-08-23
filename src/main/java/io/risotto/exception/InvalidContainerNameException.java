40f287e45471dfc05d141dde944ee3d0ad9acc35
Attila Bagossy <bagossyattila@outlook.com>
2016-08-23 23:22:42 +0200

    Documented io.risotto.exception

--- src/main/java/io/risotto/exception/InvalidContainerNameException.java
+++ src/main/java/io/risotto/exception/InvalidContainerNameException.java
@@ -1,16 +1,28 @@
 package io.risotto.exception;
 
+/**
+ * This kind of exception occurs when a specific child container name is already assigned to another
+ * child container.
+ */
 public class InvalidContainerNameException extends Exception {
   private static final String MESSAGE = "The desired container name is already in use: ";
 
   private final String name;
 
+  /**
+   * Constructs a new instance with the colliding name.
+   * @param name the name that's already taken
+   */
   public InvalidContainerNameException(String name) {
     super(MESSAGE + name);
 
     this.name = name;
   }
 
+  /**
+   * Gets the colliding name.
+   * @return the name that's already taken
+   */
   public String getName() {
     return name;
   }
--- src/main/java/io/risotto/exception/ScopeInstantiationException.java
+++ src/main/java/io/risotto/exception/ScopeInstantiationException.java
@@ -2,15 +2,28 @@ package io.risotto.exception;
 
 import io.risotto.binding.scope.Scope;
 
+/**
+ * {@code ScopeInstantiationException} is thrown when a specific scope could not been instantiated
+ * for a binding.
+ */
 public class ScopeInstantiationException extends RuntimeException {
   private final Class<? extends Scope> scopeClass;
 
+  /**
+   * Constructs a new instance with the causing {@code Scope} class and exception.
+   * @param scopeClass the scope {@code Risotto} failed to instantiate
+   * @param e the causing exception
+   */
   public ScopeInstantiationException(Class<? extends Scope> scopeClass, Exception e) {
     super("Could not instantiate scope.", e);
 
     this.scopeClass = scopeClass;
   }
 
+  /**
+   * Gets the causing {@code Scope} class.
+   * @return the scope class
+   */
   public Class<? extends Scope> getScopeClass() {
     return scopeClass;
   }
