ca203bff81b4bd895daca3906e768c76ab6026e5
Attila Bagossy <bagossyattila@outlook.com>
2016-08-26 22:44:08 +0200

    Created skeleton for BindingConfigurator

--- src/main/java/io/risotto/configurator/BindingConfigurator.java
+++ src/main/java/io/risotto/configurator/BindingConfigurator.java
@@ -1,12 +1,39 @@
 package io.risotto.configurator;
 
 import io.risotto.Container;
+import io.risotto.annotations.Binding;
+import io.risotto.binding.InstantiatableBinding;
 import io.risotto.exception.ContainerConfigurationException;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import reflection.ReflectionUtils;
 
 public class BindingConfigurator implements Configurator {
   @Override
   public void configure(Container containerInstance, Class<? extends Container> containerClass)
       throws ContainerConfigurationException {
-    
+    detectBindingMethods(containerClass)
+        .stream()
+        .map(this::createBindingFromMethod)
+        .forEach(this::addToContainer);
+  }
+
+  private InstantiatableBinding<?> createBindingFromMethod(Method method) {
+    return null;
+  }
+
+
+  private void addToContainer(InstantiatableBinding<?> binding) {
+
+  }
+
+  private List<Method> detectBindingMethods(Class<? extends Container> containerClass) {
+    return Arrays.stream(containerClass.getDeclaredMethods())
+        .filter(m -> m.isAnnotationPresent(Binding.class))
+        .filter(ReflectionUtils::isMethodInjectable)
+        .collect(Collectors.toList());
   }
 }
