0c91226bd909b2fb0916d0b16431d0ff913de33f
Attila Bagossy <bagossyattila@outlook.com>
2016-08-27 12:23:17 +0200

    Added documentation to MethodInstantiator and smaller fixes

--- src/main/java/io/risotto/instantiation/MethodInstantiator.java
+++ src/main/java/io/risotto/instantiation/MethodInstantiator.java
@@ -1,7 +1,7 @@
 package io.risotto.instantiation;
 
 import io.risotto.Container;
-import io.risotto.binding.InstantiatableBinding;
+import io.risotto.annotations.Binding;
 import io.risotto.dependency.Dependency;
 import io.risotto.dependency.DependencyDetector;
 import io.risotto.dependency.DependencyInjector;
@@ -10,12 +10,20 @@ import io.risotto.dependency.processor.ProcessorChain;
 import io.risotto.exception.DependencyDetectionException;
 import io.risotto.exception.InstantiationFailedException;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Instantiator that uses a binding supplier method to create a new instance. Binding supplier
+ * methods are methods declared in {@link Container} classes and annotated with the {@link Binding}
+ * annotation. By convention binding supplier methods should return a new instance of their return
+ * type on each invocation. Method parameters are detected and injected as dependencies.
+ * @param <T> the type of the object to be instantiated
+ */
 public class MethodInstantiator<T> implements Instantiator<T> {
   private final Class<T> instantiatedClass;
 
@@ -25,6 +33,13 @@ public class MethodInstantiator<T> implements Instantiator<T> {
 
   private DependencyInjector<T> injector;
 
+  /**
+   * Constructs a new {@code MethodInstantiator} that prodcues instances of the specified class
+   * by calling the passed {@code Method} on the passed {@code Container}.
+   * @param instantiatedClass the class of the class that will be supplied by the method
+   * @param container the {@code Container} in which the used method was declared
+   * @param method the method that will supply the instance
+   */
   public MethodInstantiator(Class<T> instantiatedClass, Container container, Method method) {
     this.instantiatedClass = instantiatedClass;
 
@@ -45,9 +60,8 @@ public class MethodInstantiator<T> implements Instantiator<T> {
 
   @Override
   public List<Dependency<?>> getImmediateDependencies() {
-    MethodDependencyDetector<T>
-        detector =
-        new MethodDependencyDetector<T>(instantiatedClass, method);
+    MethodDependencyDetector detector =
+        new MethodDependencyDetector(instantiatedClass);
 
     Optional<List<Dependency<?>>> dependencies = detector.detectImmediateDependencies();
 
@@ -56,7 +70,7 @@ public class MethodInstantiator<T> implements Instantiator<T> {
     }
 
     injector =
-        new MethodDependencyInjector<>(instantiatedClass, dependencies.get(), container, method);
+        new MethodDependencyInjector(instantiatedClass, dependencies.get());
 
     return dependencies.get();
   }
@@ -66,13 +80,9 @@ public class MethodInstantiator<T> implements Instantiator<T> {
     return instantiatedClass;
   }
 
-  private class MethodDependencyDetector<K> extends DependencyDetector<K> {
-    private final Method method;
-
-    public MethodDependencyDetector(Class<K> clazz, Method method) {
+  private class MethodDependencyDetector extends DependencyDetector<T> {
+    MethodDependencyDetector(Class<T> clazz) {
       super(clazz);
-
-      this.method = method;
     }
 
     @Override
@@ -95,24 +105,14 @@ public class MethodInstantiator<T> implements Instantiator<T> {
     }
   }
 
-  private class MethodDependencyInjector<T> extends DependencyInjector<T> {
+  private class MethodDependencyInjector extends DependencyInjector<T> {
     private final List<Dependency<?>> dependencies;
 
-    private final Container container;
-
-    private final Method method;
-
-    public MethodDependencyInjector(Class<T> instantiatableClass,
-                                    List<Dependency<?>> dependencies,
-                                    Container container,
-                                    Method method) {
+    MethodDependencyInjector(Class<T> instantiatableClass,
+                             List<Dependency<?>> dependencies) {
       super(instantiatableClass);
 
       this.dependencies = dependencies;
-
-      this.container = container;
-
-      this.method = method;
     }
 
     @Override
@@ -127,8 +127,9 @@ public class MethodInstantiator<T> implements Instantiator<T> {
 
         Object instance = method.invoke(container, bindingResults.toArray());
 
-        return (T)instance;
-      } catch (Exception e) {
+        return (T) instance;
+      } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException |
+          NullPointerException e) {
         throw new InstantiationFailedException(instantiatableClass, e);
       }
     }
