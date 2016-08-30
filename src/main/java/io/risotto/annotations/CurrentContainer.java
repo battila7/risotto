84a374314a5bf721eecd9ddc11f934ac3ef88d6a
Attila Bagossy <bagossyattila@outlook.com>
2016-08-30 19:35:37 +0200

    Implemented the configure method() and added documentation

--- src/main/java/io/risotto/annotations/CurrentContainer.java
+++ src/main/java/io/risotto/annotations/CurrentContainer.java
@@ -1,4 +1,20 @@
 package io.risotto.annotations;
 
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Inject specifier annotation that can be used to mark a target to current container instance
+ * injection.
+ * @see io.risotto.configurator.CurrentContainerConfigurator;
+ */
+@InjectSpecifier
+@Target({ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Documented
 public @interface CurrentContainer {
 }
--- src/main/java/io/risotto/configurator/CurrentContainerConfigurator.java
+++ src/main/java/io/risotto/configurator/CurrentContainerConfigurator.java
@@ -1,12 +1,45 @@
 package io.risotto.configurator;
 
+import static io.risotto.binding.BasicBinding.bind;
+
 import io.risotto.Container;
+import io.risotto.annotations.CurrentContainer;
+import io.risotto.binding.InstantiatableBinding;
 import io.risotto.exception.ContainerConfigurationException;
 
+import java.lang.reflect.Method;
+
+/**
+ * Configurator that adds a binding wrapping the currently configurated container instance. The
+ * binding is an annotated binding using the {@link io.risotto.annotations.CurrentContainer}
+ * annotation.
+ */
 public class CurrentContainerConfigurator implements Configurator {
+  private static final String ADD_BINDING_METHOD_NAME = "addBinding";
+
   @Override
   public void configure(Container containerInstance, Class<? extends Container> containerClass)
       throws ContainerConfigurationException {
+    InstantiatableBinding<Container> binding =
+        bind(Container.class).withAnnotation(CurrentContainer.class).toInstance(containerInstance);
 
+    addToContainer(containerInstance, containerClass, binding);
+  }
+
+  private void addToContainer(Container container, Class<? extends Container> containerClass,
+                              InstantiatableBinding<?> binding)
+      throws ContainerConfigurationException {
+    try {
+      Class<?> superClass = containerClass.getSuperclass();
+
+      Method addBindingMethod =
+          superClass.getDeclaredMethod(ADD_BINDING_METHOD_NAME, InstantiatableBinding.class);
+
+      addBindingMethod.setAccessible(true);
+
+      addBindingMethod.invoke(container, binding);
+    } catch (SecurityException | IllegalArgumentException | ReflectiveOperationException e) {
+      throw new ContainerConfigurationException(container, e);
+    }
   }
 }
