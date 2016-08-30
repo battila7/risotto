1cd016300528adbca89285427443f1bc2404ce27
Attila Bagossy <bagossyattila@outlook.com>
2016-08-30 19:46:29 +0200

    Changed annotation target to FIELD/METHOD/PARAMETER

--- src/main/java/io/risotto/annotations/CurrentContainer.java
+++ src/main/java/io/risotto/annotations/CurrentContainer.java
@@ -1,6 +1,8 @@
 package io.risotto.annotations;
 
-import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
@@ -13,7 +15,7 @@ import java.lang.annotation.Target;
  * @see io.risotto.configurator.CurrentContainerConfigurator
  */
 @InjectSpecifier
-@Target({ANNOTATION_TYPE})
+@Target({PARAMETER, FIELD, METHOD})
 @Retention(RUNTIME)
 @Documented
 public @interface CurrentContainer {
